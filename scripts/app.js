feb1f982008b1552c24bd9b5c6e398df7cae1880
Habibur Rahman <habib.swi16@gmail.com>
2023-09-01 01:38:55 +0600

    card template

--- scripts/app.js
+++ scripts/app.js
@@ -12,10 +12,62 @@ const handleCategoryBtn = async () => {
 
     categoryBtn.classList = `btn btn-accent capitalize active:btn-error`;
 
-    categoryBtn.textContent = category.category;
+    // categoryBtn.textContent = category.category;
+    categoryBtn.innerHTML = `
+    <div onclick="handleCategoryContent(${category.category_id})">${category.category}</div>
+    `;
 
     categoryContainer.appendChild(categoryBtn);
   });
 };
 
+const handleCategoryContent = async (categoryId) => {
+  const response = await fetch(
+    ` https://openapi.programming-hero.com/api/videos/category/${categoryId}`
+  );
+  const data = await response.json();
+  const content = data.data;
+
+  // console.log(content);
+  const contentContainer = document.getElementById("content-container");
+
+  content.forEach((card) => {
+    const contentCard = document.createElement("div");
+
+    contentCard.innerHTML = `
+    <div class="relative mt-2">
+    <img src="${
+      card.thumbnail
+    }" alt="" class="rounded-lg w-[312px] h-[200px]" />
+
+    <p class="absolute bg-black text-white text-[10px]">timestamp</p>
+
+    <div class="flex mt-5 gap-3">
+      <img src="${
+        card.authors[0].profile_picture
+      }" alt="" class="rounded-full w-8 h-8" />
+
+      <div>
+        <h1 class="text-black font-bold">${card.title}</h1>
+
+        <div class="flex">
+          <p class="my-2 text-[#171717B2] text-sm">${
+            card.authors[0].profile_name
+          }</p>
+
+          <p>${card.authors[0].verified ? "images/blueTick.png" : ""}</p>
+        </div>
+
+        <p class="text-[#171717B2] font-sm mt-2">${card.others.views} views</p>
+      </div>
+    </div>
+  </div>
+    `;
+
+    contentContainer.appendChild(contentCard);
+    console.log(card.authors[0].profile_name);
+  });
+};
+
 handleCategoryBtn();
+handleCategoryContent(1000);
